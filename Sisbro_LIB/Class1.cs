5904ed7fa4e5565a8d0a4828531279312adb4fd0
Avorycado <s160421046@student.ubaya.ac.id>
2023-04-03 20:00:52 +0700

    p

---
+++ Sisbro_LIB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sisbro_LIB
+{
+    public class Class1
+    {
+    }
+}
